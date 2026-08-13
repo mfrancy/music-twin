9f0f725c7368812ecd868e0fc57897ef638e1c76
Mariana <marianalopes697@gmail.com>
2026-08-13 14:58:46 -0300

    feat: enhance comparison page with user profiles and detailed stats display

--- src/app/features/comparison/pages/comparison/comparison-page.ts
+++ src/app/features/comparison/pages/comparison/comparison-page.ts
@@ -1,16 +1,16 @@
-import { Component, EventEmitter, inject, output } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import { DecimalPipe } from '@angular/common';
 import { ComparisonInput } from '../../models/comparison-input';
-import { form } from '@angular/forms/signals';
 import { ComparisonForm } from '../../components/comparison/comparison-form';
 import { LastfmService } from '../../services/lastfm.service';
 import { forkJoin } from 'rxjs';
 import { ComparisonService } from '../../services/comparison.service';
 import { ComparisonStats } from '../../models/comparison-stats.interface';
+import { UserProfile } from '../../models/user-profile.interface';
 
 @Component({
   selector: 'app-comparison-page',
-  imports: [ReactiveFormsModule, ComparisonForm],
+  imports: [ComparisonForm, DecimalPipe],
   templateUrl: './comparison-page.html',
   styleUrl: './comparison-page.scss',
 })
@@ -19,6 +19,8 @@ export class ComparisonPage {
   lastfmService = inject(LastfmService);
   comparisonService = inject(ComparisonService);
   comparisonStats: ComparisonStats | null = null 
+  userProfile: UserProfile | null = null;
+  otherUserProfile: UserProfile | null = null;
 
   onCompare(event: ComparisonInput) {
     const user$ = this.lastfmService.getUserInfo(event.user);
@@ -33,6 +35,8 @@ export class ComparisonPage {
         const otherUser = response.otherUser;
         const comparision = this.comparisonService.compare(user, otherUser)
         console.log(comparision)
+        this.userProfile = user;
+        this.otherUserProfile = otherUser;
         this.comparisonStats = comparision
       }, error: err => {
         console.log(err)
