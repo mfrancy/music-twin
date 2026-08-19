82df0c4576356c565490f97bc61ccf6324b2a881
Mariana <marianalopes697@gmail.com>
2026-08-18 22:15:59 -0300

    feat: enhance comparison feature with user profiles and improved UI
    - Updated ArtistsComparisonComponent to utilize Angular signals and computed properties for better state management.
    - Refactored comparison-results component to display user statistics more clearly and added user profile inputs.
    - Improved styling for comparison results and user profiles for a more cohesive design.
    - Removed unnecessary elements from comparison form and adjusted layout for better responsiveness.
    - Updated main layout to simplify navigation and enhance visual hierarchy.
    - Added global styles for smoother scrolling and consistent background across the application.

--- src/app/features/comparison/components/artists-comparison/artists-comparison.ts
+++ src/app/features/comparison/components/artists-comparison/artists-comparison.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input, signal } from '@angular/core';
 import { DecimalPipe } from '@angular/common';
 import { Artist } from '../../models/artists.interface';
 
@@ -12,4 +12,28 @@ export class ArtistsComparisonComponent {
   userArtists = input<Artist[]>([]);
   otherUserArtists = input<Artist[]>([]);
   commonArtists = input<Artist[]>([]);
+  userName = input('Usuário A');
+  otherUserName = input('Usuário B');
+  commomLimit: number = 5;
+  selectedLimit = signal<number>(5);
+
+
+
+  visibleUserArtists = computed(() => {
+    return this.userArtists().slice(0, this.selectedLimit())
+  });
+
+  visibleOtherUserArtists = computed(() => {
+    return this.otherUserArtists().slice(0, this.selectedLimit())
+  });
+
+  onLimitChange(event: Event) {
+    const selected = (event.target as HTMLInputElement).value
+    this.selectedLimit.set(Number(selected));
+  }
+
+  visibleCommomArtists = computed(() => {
+    return this.commonArtists().slice(0, this.commomLimit);
+  })
+
 }
